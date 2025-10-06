84e8291ef8e95fd482ac23ac4b0b2c8234e557d8
Jackyjhs <2512542019@mail.utec.edu.sv>
2025-10-06 10:07:52 -0600

    Initial project structure created after starting over

---
+++ DistribuidoraDonPepeWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DistribuidoraDonPepeWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
